1ec730ef3662d6c9f12ee9c59be32e55ffe2e475
Chris Rudel <crudel@stevens.edu>
2019-10-27 22:06:01 -0400

    game design unity intro project

---
+++ har260/InClass01/Assets/Scripts/personClicked.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class personClicked : MonoBehaviour
+{
+
+    public GameObject background;
+    public GameObject oar;
+    public Sprite docks;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        background.GetComponent<SpriteRenderer>().sprite = docks;
+        this.enabled = false;
+        oar.SetActive(true);
+        Debug.Log("hey");
+    }
+}
